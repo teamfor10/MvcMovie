b9073152312e640d9f7c186dace2db2cce22baec
Asa Staven <asa.jue.staven@hpe.com>
2017-01-13 10:29:47 -0700

    Added a model and view

---
+++ src/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: /HelloWorld/
+        // invoked by appending /HelloWorld/ to the base url
+        public IActionResult Index()
+        {
+            var Mov = new MovieDatabase();
+            return View(Mov);
+        }
+
+    }
+   
+}
---
+++ src/MvcMovie/Models/Movies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    /*public class Movies
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }*/
+    public class Movies
+    {
+        public int Rating { get; set; }
+        public string Movie { get; set; }
+    }
+
+    public class MovieDatabase : List<Movies>
+    {
+
+        public MovieDatabase()
+        {
+            Add(new Movies() { Rating = 3, Movie = "101 Dalmations" });
+            Add(new Movies() { Rating = 5, Movie = "LOTR" });
+            Add(new Movies() { Rating = 1, Movie = "Sharknado" });
+        }
+    }
+
+}
